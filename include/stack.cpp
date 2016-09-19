ad0cb29abb6a4b4c8b73daab10a9647fc9b96525
DANTEpolaris <z.abraam@yandex.ru>
2016-09-19 14:49:42 +0600

    Update stack.cpp
    rename copy_ -> new copy, выненсение new copy

--- include/stack.cpp
+++ include/stack.cpp
@@ -3,6 +3,13 @@
 #pragma once
 #include <iostream>
 using namespace std;
+template<typename>
+auto newcopy(T * item, size_t size, size_t count) -> T*
+{
+	T * buff = new T[size];
+	copy(item, item + count, buff);
+	return buff;	
+}
 template <typename T>
 class stack
 {
@@ -12,7 +19,6 @@ public:
 	~stack();
 	size_t count() const;
 	auto push(T const &) -> void;
-	auto copy_(T * item, size_t size, size_t count) -> T*;
 	T pop();
 	auto operator=(stack const & right)->stack &;
 private:
@@ -20,23 +26,19 @@ private:
 	size_t array_size_;
 	size_t count_;
 };
-
-
 template <typename T>
 size_t stack<T>::count() const
 {
 	return count_;
 }
 template <typename T>
-stack<T>::stack()
+stack<T>::stack():array_size_(0), count(0), array_(new T[array_size_])
 {
-	array_size_ = 0;
-	array_ = new T[array_size_];
-	count_ = 0;
+
 }
 
 template<typename T>
-stack<T>::stack(stack const & item):array_size_(item.array_size_), count_(item.count_),array_(copy_(item.array_, item.array_size_,item.count_))
+stack<T>::stack(stack const & item):array_size_(item.array_size_), count_(item.count_),array_(newcopy(item.array_, item.array_size_,item.count_))
 {
 
 }
@@ -52,17 +54,11 @@ auto stack<T>::push(T const & item) -> void {
 	if (count_ == array_size_) {
 		size_t size = array_size_ * 2 + (array_size_ == 0) ;
 		delete[] array_;
-		array_ = copy_(array_, size, array_size_);
+		array_ = newcopy(array_, size, array_size_);
 		array_size_ = size;
 	}
 	++count_;
 	array_[count_ - 1] = item;
-}
-template<typename T>
-auto stack<T>::copy_(T * item, size_t size, size_t count) -> T* {
-	T * buff = new T[size];
-	copy(item, item + count, buff);
-	return buff;
 }
 
 template<typename T>
@@ -78,7 +74,7 @@ auto stack<T>::operator=(stack const & right) -> stack & {
 	delete[] array_;
 	count_ = right.count_;
 	array_size_ = right.array_size_;
-	copy_(right.array_, array_size_, count_);
+	newcopy(right.array_, array_size_, count_);
 	}
 	return *this;
 }
