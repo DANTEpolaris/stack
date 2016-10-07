bec5b30c3c2a2c00673219c231d2004b99b0c950
DANTEpolaris <z.abraam@yandex.ru>
2016-10-07 19:03:03 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -13,23 +13,23 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.pop()==1);
+  REQUIRE(s.top()==1);
 }
-/*
+
 SCENARIO("top", "[top]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
   REQUIRE(s.top()==1);
 }
-*/
+
 SCENARIO("pop", "[pop]"){
   stack<int> s;
   s.push(1);
   s.push(2);
   s.pop();
   REQUIRE(s.count()==1);
-  REQUIRE(s.pop()==1);
+  REQUIRE(s.top()==1);
 }
 
 
@@ -39,21 +39,20 @@ SCENARIO("prisv", "[prisv]"){
   stack<int> s2;
   s2=s;
   REQUIRE(s.count()==1);
-  REQUIRE(s.pop()==1);
+  REQUIRE(s.top()==1);
 }
 SCENARIO("copy", "[copy]"){
   stack<int> s;
   s.push(1);
   stack <int> a = s;
   REQUIRE(a.count()==1);
-  REQUIRE(a.pop()==1);
+  REQUIRE(a.top()==1);
 }
 SCENARIO("test", "[test]"){
   stack<int> s;
   REQUIRE(s.count()==0);
-}/*
+}
 SCENARIO("empty", "[empty]"){
   stack<int> s;
   REQUIRE(s.empty()==true);
 }
-*/
