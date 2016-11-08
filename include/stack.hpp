f9cfd4e5876c8d4bdb359252b83167dcb980c112
DANTEpolaris <z.abraam@yandex.ru>
2016-11-08 10:27:44 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -166,7 +166,6 @@ template<typename T>
 auto allocator<T>::destroy(T * first, T * last) -> void
 {	if(first>=ptr_&&last<=ptr_+this->count())
 	for (; first != last; ++first) {
-		if(!map_->test(first))
 		destroy(&*first);
 	}
 }
