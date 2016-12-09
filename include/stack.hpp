94283cd21868fb432db0e6ae7afbab34657a4fac
DANTEpolaris <z.abraam@yandex.ru>
2016-12-09 21:24:27 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -224,7 +224,7 @@ stack<T>::stack(size_t size) :allocator_(size), m() {}
 
 template <typename T>
 stack<T>::stack(stack const & other) : allocator_(0), m() {
-std::lock_guard<std::mutex> lk2(other.m);
+std::lock_guard<std::mutex> locker(other.m);
 allocator_.swap(allocator<T>(other.allocator_));
 }
 template <typename T>
