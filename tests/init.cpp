3866ebb1375f073d376dd67a9c488ea674af5758
DANTEpolaris <z.abraam@yandex.ru>
2016-12-09 21:51:27 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,7 +1,6 @@
-#include "stack.hpp"
+#include <stack.cpp>
 #include <catch.hpp>
 #include <iostream>
-#include <thread>
 using namespace std;
 
 SCENARIO("count", "[count]"){
@@ -14,27 +13,21 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
 }
 
 SCENARIO("pop", "[pop]"){
   stack<int> s;
-  s.push(1); s.pop();
+  s.push(1);
+  REQUIRE(*(s.pop())==1);	
   REQUIRE(s.count()==0);
 }
 
-SCENARIO("cop", "[cop]"){
+SCENARIO("prisv", "[prisv]"){
   stack<int> s;
   s.push(1);
-  stack<int> s2=s;
+  stack<int> s2;
+  s2=s;
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
-}
-
-SCENARIO("top", "[top]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.top()==1);
 }
 
 SCENARIO("empty", "[empty]"){
