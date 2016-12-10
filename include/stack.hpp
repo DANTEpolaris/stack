732fb640a6ef81267f63403329d9a1d66ce42c93
DANTEpolaris <z.abraam@yandex.ru>
2016-12-10 12:25:10 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -213,9 +213,9 @@ size_t stack<T>::count() const
 }
 
 template<typename T>
-stack<T>::stack(stack other & other): allocator_(other.allocator_), m(){}
+stack<T>::stack(stack const & other): allocator_(other.allocator_), m(){};
 template <typename T>
-stack<T>::stack(size_t size) ::allocator_(size), m() {} 
+stack<T>::stack(size_t size):allocator_(size), m() {} ;
 
 template <typename T>
 void stack<T>::push(T const &item) {
